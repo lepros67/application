8660f4ee3251ad80b908f4e2edf810d6aed9eac6
lepros67 <florian.scheeg@gmail.com>
2025-12-23 21:35:51 +0100

    Modification du bouton

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:untestapp/pages/add_event_page.dart';
+import 'package:untestapp/pages/event_page.dart';
 import 'package:untestapp/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+
+  int _currentIndex = 0;
+
+  void setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Appli nulle"),
+          title: const Text("Appli nulle"),
         ),
-        body: HomePage(),
+
+        body: [
+          HomePage(setCurrentIndex: setCurrentIndex),
+          const EventPage(),
+          const AddEventPage()
+        ][_currentIndex],
+
         bottomNavigationBar: BottomNavigationBar(
-            items: [
 
+          currentIndex: _currentIndex,
+          onTap: (index) => setCurrentIndex(index),
 
-              BottomNavigationBarItem(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          backgroundColor: Colors.blue,
+          iconSize: 32,
+          elevation: 10,
+          selectedFontSize: 16,
+          unselectedFontSize: 16,
+
+          items: const [
+            BottomNavigationBarItem(
                 icon: Icon(Icons.home),
                 label: 'Accueil'
-              ),
+            ),
 
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.list),
-                  label: 'Liste'
-              )
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'Liste'
+            ),
 
-
-            ],
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: 'Ajout'
+            )
+          ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'event_page.dart';
-
-
 class HomePage extends StatelessWidget {
+  final void Function(int) setCurrentIndex;
+
   const HomePage({
     super.key,
+    required this.setCurrentIndex,
   });
 
   @override
@@ -30,22 +30,18 @@ class HomePage extends StatelessWidget {
               ),
             ),
 
-            Padding(padding: EdgeInsets.only(top: 20)),
+            const Padding(padding: EdgeInsets.only(top: 20)),
 
             ElevatedButton(
-                style: ButtonStyle(
+                style: const ButtonStyle(
                   padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
                   backgroundColor: WidgetStatePropertyAll(Colors.blue),
                 ),
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                          pageBuilder: (_, __, ___) => EventPage()
-                      )
-                  );
+                  setCurrentIndex(1);
+                  print("bouton cliqué");
                 },
-                child : const Text("Un gros bouton",
+                child: const Text("Un gros bouton",
                   style: TextStyle(
                       fontSize: 20,
                       color: Colors.black
